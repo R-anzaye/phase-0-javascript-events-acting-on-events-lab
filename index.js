461533f6e23b1cac55cb111a4e21f01d86e296f1
rileydollar8@gmail.com <ryan.anzaye@student.moringaschool.com>
2024-04-04 17:10:39 +0300

    enets

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+let  dodger = document.querySelector('#dodger');
+
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseFloat(leftNumbers, 10)
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+
+function moveDodgerRight() {
+var leftNumbers = dodger.style.left.replace('px', '');
+var left = parseFloat(leftNumbers, 10)
+if (left < 360) {
+  dodger.style.left = `${left + 1}px`
+}
+}
+
+document.addEventListener('keydown', (e)=>{
+if (e.key === "ArrowLeft") {
+  moveDodgerLeft()
+}
+})
+
+document.addEventListener('keydown', (e)=>{
+if (e.key === "ArrowRight") {
+  moveDodgerRight()
+}
+})
